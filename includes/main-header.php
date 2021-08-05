9042e974c9374abf4a2e7650cb4b14c52132e9ef
Mansi Patel <mansipatel09v@gmail.com>
2021-08-06 04:50:44 +0530

    First commit

---
+++ includes/main-header.php
@@ -0,0 +1,54 @@
+<?php 
+
+ if(isset($_Get['action'])){
+		if(!empty($_SESSION['cart'])){
+		foreach($_POST['quantity'] as $key => $val){
+			if($val==0){
+				unset($_SESSION['cart'][$key]);
+			}else{
+				$_SESSION['cart'][$key]['quantity']=$val;
+			}
+		}
+		}
+	}
+?>
+	<div class="main-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+					<!-- ============================================================= LOGO ============================================================= -->
+<div class="logo">
+	<a href="index.php">
+		
+		<h2>Books and More</h2>
+
+	</a>
+</div>		
+</div>
+<!--<div class="col-xs-12 col-sm-12 col-md-6 top-search-holder">
+<div class="search-area">
+    <form name="search" method="post" action="search-result.php">
+        <div class="control-group">
+
+            <input class="search-field" placeholder="Search here..." name="product" required="required" />
+
+            <button class="search-button" type="submit" name="search"></button>    
+
+        </div>
+    </form>
+</div><!-- /.search-area -->
+<!-- ============================================================= SEARCH AREA : END ============================================================= -->				</div><!-- /.top-search-holder -->
+
+				<div class="col-xs-12 col-sm-12 col-md-3 animate-dropdown top-cart-row">
+					<!-- ============================================================= SHOPPING CART DROPDOWN ============================================================= -->
+
+
+
+
+
+<!-- ============================================================= SHOPPING CART DROPDOWN : END============================================================= -->				</div><!-- /.top-cart-row -->
+			</div><!-- /.row -->
+
+		</div><!-- /.container -->
+
+	</div>
